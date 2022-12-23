d20700ef5a42cbc483999f2327468e80588ab0bc
surajnakhale810 <surajnakhale810@gmail.com>
2022-12-23 12:43:50 +0530

    calculator1

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+let input = document.getElementById("input")
+let expression = '';
+function solve(e){
+// console.log(e)
+if(e === 'clear'){
+    expression = "";
+    // console.log(expression)
+    input.value = expression
+}
+else if(e === 'ans'){
+    let result = eval(expression)
+    input.value = result
+}
+else{
+    expression = expression + e
+    input.value = expression;
+}
+}
